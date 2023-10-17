1f1433379f79cfd47972e00558a22b24e8d44110
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2023-10-17 21:42:45 +0300

    done 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const local = 'feedback-form-state';
+
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(local)) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInputData(e) {
+    dataForm = { email: email.value, message: message.value };
+    localStorage.setItem(local, JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+    if (dataForm) {
+        email.value = dataForm.email || '';
+        message.value = dataForm.message || '';
+    }
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    console.log({ email: email.value, message: message.value });
+
+    if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+}
+
+    localStorage.removeItem(local);
+    e.currentTarget.reset();
+    dataForm = {};
+}
